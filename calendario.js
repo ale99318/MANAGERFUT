2b54f5d94510e4369a2291d945f07cdfc4c30b10
ale99318 <jesusjironrodriguez155@gmail.com>
2025-07-07 15:25:09 -0500

    Delete calendario.js

--- calendario.js
+++
@@ -1,5 +0,0 @@
-// calendario.js
-document.addEventListener("DOMContentLoaded", () => {
-  const fechaActual = parseInt(localStorage.getItem("fechaActual")) || 1;
-  document.getElementById("fechaActual").textContent = `Jornada ${fechaActual}`;
-});
